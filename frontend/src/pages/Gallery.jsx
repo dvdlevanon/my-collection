d4e729440a34bd78395f13d48c5956c04c801dcc
David Levanon <dvdlevanon@gmail.com>
2023-01-11 21:17:51 +0200

    fix recoil

--- frontend/src/pages/Gallery.jsx
+++ frontend/src/pages/Gallery.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import React from 'react'
+import { atom, useRecoilState } from 'recoil'
 import '../styles/Tag.css';
 import '../styles/Items.css';
 import '../styles/Pages.css';
@@ -7,7 +8,6 @@ import ItemsList from "../components/Items";
 import SuperTags from '../components/SuperTags';
 import Tags from '../components/Tags';
 import SidePanel from '../components/SidePanel';
-import { useRecoilState, atom } from 'recoil';
 
 const tagsAtom = atom({
 	key: "tags",
@@ -19,21 +19,6 @@ const itemsAtom = atom({
 	default: []
 });
 
-const selectedSuperTagAtom = atom({
-	key: "selectedSuperTag",
-	default: null
-});
-
-const activeTagsAtom = atom({
-	key: "activeTags",
-	default: []
-});
-
-const selectedTagsAtom = atom({
-	key: "selectedTags",
-	default: []
-});
-
 const conditionTypeAtom = atom({
 	key: "conditionType",
 	default: "||"
@@ -42,23 +27,51 @@ const conditionTypeAtom = atom({
 function Gallery() {
     let [tags, setTags] = useRecoilState(tagsAtom);
 	let [items, setItems] = useRecoilState(itemsAtom);
-	let [selectedSuperTag, setSelectedSuperTag] = useRecoilState(selectedSuperTagAtom);
-	let [activeTags, setActiveTags] = useRecoilState(activeTagsAtom);
-	let [selectedTags, setSelectedTags] = useRecoilState(selectedTagsAtom);
 	let [conditionType, setConditionType] = useRecoilState(conditionTypeAtom);
 
 	useEffect(() => {
-		if (items.length == 0) {
-			fetch('http://localhost:8080/items')
-				.then((response) => response.json())
-				.then((items) => setItems(items));
+		if (tags.length != 0) {
+			return;
 		}
-		if (tags.length == 0) {
-			fetch('http://localhost:8080/tags')
-				.then((response) => response.json())
-				.then((tags) => setTags(tags));
-		}
+
+		fetch('http://localhost:8080/tags')
+			.then((response) => response.json())
+			.then((tags) => setTags(tags));
 	}, []);
+
+	useEffect(() => {
+		if (items.length != 0) {
+			return;
+		}
+
+		fetch('http://localhost:8080/items')
+			.then((response) => response.json())
+			.then((items) => setItems(items));
+	}, []);
+
+	const getSelectedTags = () => {
+		return tags.filter((tag) => {
+			return tag.selected;
+		})
+	}
+
+	const getActiveTags = () => {
+		return tags.filter((tag) => {
+			return tag.active;
+		})
+	}
+
+	const getSelectedSuperTag = () => {
+		let selectedSupertTags = tags.filter((tag) => {
+			return tag.selected && !tag.parentId;
+		})
+
+		if (selectedSupertTags.length > 0) {
+			return selectedSupertTags[0];
+		}
+
+		return null;
+	}
 
 	const getSeletedItems = (selectedTags) => {
 		if (selectedTags.length === 0) {
@@ -89,84 +102,72 @@ function Gallery() {
 	};
 
 	const onSuperTagSelected = (superTag) => {
-		superTag = updateTag(superTag, (tag) => { 
-			tag.selected = true;
-			return tag;
-		});
-
-		setSelectedSuperTag(superTag);
+		updateTag(superTag, (superTag) => {
+			superTag.selected = true;
+			return superTag;
+		})
 	};
 
 	const onSuperTagDeselected = (superTag) => {
-		superTag = updateTag(superTag, (tag) => { 
-			tag.selected = false;
-			return tag;
-		});
-
-		setSelectedSuperTag(null)
+		updateTag(superTag, (superTag) => {
+			superTag.selected = false;
+			return superTag;
+		})
 	};
 
 	const onTagActivated = (tag) => {
-		onSuperTagDeselected(selectedSuperTag)
+		onSuperTagDeselected(getSelectedSuperTag())
 
 		if (tag.active) {
 			return
 		}
 
-		tag = updateTag(tag, (tag) => { 
+		updateTag(tag, (tag) => { 
 			tag.active = true;
 			tag.selected = true;
 			return tag;
 		});
-
-		setActiveTags([...activeTags, tag])
-		setSelectedTags([...selectedTags, tag])
 	}
 
 	const onTagDeactivated = (tag) => {
-		tag = updateTag(tag, (tag) => { 
+		updateTag(tag, (tag) => { 
 			tag.active = false;
-			tag.selected = false;
+			tag.selected = false; 
 			return tag;
 		});
-
-		setActiveTags(activeTags.filter((cur) => cur.id != tag.id ))
-		setSelectedTags(selectedTags.filter((cur) => cur.id != tag.id ))
 	}
 	
 	const updateTag = (tag, updater) => {
-		let newTags = tags.map((cur) => {
-			if (tag.id == cur.id) {
-				return updater({...cur})
-			}
+		setTags((tags) => { 
+			return tags.map((cur) => {
+				if (tag.id == cur.id) {
+					return updater({...cur})
+				}
 
-			return cur
-		})
-
-		setTags(newTags);
-		return newTags.filter((cur) => cur.id == tag.id)[0]
+				return cur
+			})
+		});
 	}
 
 	const onTagSelected = (tag) => {
-		tag = updateTag(tag, (tag) => { 
-			tag.selected = true;
+		updateTag(tag, (tag) => { 
+			tag.selected = true 
 			return tag;
 		})
-		setSelectedTags([...selectedTags, tag])
 	}
 
 	const onTagDeselected = (tag) => {
-		tag = updateTag(tag, (tag) => {
-			tag.selected = false;
+		updateTag(tag, (tag) => { 
+			tag.selected = false 
 			return tag;
 		})
-		setSelectedTags(selectedTags.filter((cur) => cur.id != tag.id ))
 	}
 
 	const getTags = (superTag) => {
 		if (!superTag.children) {
 			return [];
 		}
+
 		let children = superTag.children.map((tag) => {
 			return tags.filter((cur) => {
 				return cur.id == tag.id;
@@ -190,12 +191,12 @@ function Gallery() {
 				onSuperTagDeselected={onSuperTagDeselected}
 			/>
 			<div className="center-content">
-				{selectedSuperTag ? <Tags tags={getTags(selectedSuperTag)} onTagActivated={onTagActivated} /> : ''}
-				<SidePanel activeTags={activeTags} onTagDeactivated={onTagDeactivated} 
+				{getSelectedSuperTag() ? <Tags tags={getTags(getSelectedSuperTag())} onTagActivated={onTagActivated} /> : ''}
+				<SidePanel activeTags={getActiveTags()} onTagDeactivated={onTagDeactivated} 
 					   onTagSelected={onTagSelected} onTagDeselected={onTagDeselected} onChangeCondition={onChangeCondition} />
-				<ItemsList items={getSeletedItems(selectedTags)} />
+				<ItemsList items={getSeletedItems(getSelectedTags())} />
 			</div>
-		</div>	
+		</div>
     )
 }
 
