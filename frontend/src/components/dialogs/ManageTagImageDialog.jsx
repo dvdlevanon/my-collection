920f96406f0978de06ca5ec41a827223456b4109
David Levanon <dvdlevanon@gmail.com>
2023-06-04 10:06:45 +0300

    manage tag images dialog - work in progres

--- frontend/src/components/dialogs/ManageTagImageDialog.jsx
+++ frontend/src/components/dialogs/ManageTagImageDialog.jsx
@@ -1,15 +1,31 @@
 import CloseIcon from '@mui/icons-material/Close';
 import NoImageIcon from '@mui/icons-material/HideImage';
-import { Box, Button, Dialog, DialogContent, DialogTitle, IconButton } from '@mui/material';
-import React, { useRef } from 'react';
+import { Box, Button, Dialog, DialogContent, DialogTitle, IconButton, Stack, Typography } from '@mui/material';
+import React, { useRef, useState } from 'react';
+import { useQuery, useQueryClient } from 'react-query';
+import Client from '../../utils/client';
+import ReactQueryUtil from '../../utils/react-query-util';
 import TagsUtil from '../../utils/tags-util';
+import TagImageTypeSelector from '../tag-picker/TagImageTypeSelector';
 
 function ManageTagImageDialog({ tag, onClose }) {
-	// const queryClient = useQueryClient();
+	const queryClient = useQueryClient();
+	const [tit, setTit] = useState(null);
+	const [updatedTag, setUpdatedTag] = useState(tag);
+	const titsQuery = useQuery({
+		queryKey: ReactQueryUtil.TAG_IMAGE_TYPES_KEY,
+		queryFn: Client.getTagImageTypes,
+		onSuccess: (tits) => {
+			if (!tit) {
+				setTit(tits[0]);
+			}
+		},
+	});
+
 	const fileDialog = useRef(null);
 
-	const hasImage = () => {
-		return tag.images && tag.images.length > 0;
+	const imageFromUrlClicked = (e) => {
+		e.stopPropagation();
 	};
 
 	const changeTagImageClicked = (e) => {
@@ -18,17 +34,28 @@ function ManageTagImageDialog({ tag, onClose }) {
 		fileDialog.current.click();
 	};
 
+	const updateTagImage = (tag, tit, fileUrl) => {
+		if (tag.images && tag.images.some((image) => image.imageType === tit.id)) {
+			return;
+		}
+
+		let updatedTag = { ...tag };
+		updatedTag.images = updatedTag.images || [];
+		updatedTag.images.push({ url: fileUrl, tag_id: tag.id, imageType: tit.id });
+		Client.saveTag(updatedTag).then(() => {
+			setUpdatedTag(updatedTag);
+			queryClient.refetchQueries({ queryKey: ReactQueryUtil.TAGS_KEY });
+		});
+	};
+
 	const imageSelected = (e) => {
 		if (fileDialog.current.files.length !== 1) {
 			return;
 		}
 
-		// Client.uploadFile(`tags-image/${tag.id}`, fileDialog.current.files[0], (fileUrl) => {
-		// 	Client.saveTag({ ...tag, imageUrl: fileUrl.url }).then(() => {
-		// 		onClose();
-		// 		QueryClient.refetchQueries({ queryKey: ReactQueryUtil.TAGS_KEY });
-		// 	});
-		// });
+		Client.uploadFile(`tags-image-types/${tag.id}/${tit.id}`, fileDialog.current.files[0], (fileUrl) => {
+			updateTagImage(updatedTag, tit, fileUrl.url);
+		});
 	};
 
 	const removeTagImageClicked = (e) => {
@@ -49,9 +76,15 @@ function ManageTagImageDialog({ tag, onClose }) {
 			open={true}
 			fullWidth={true}
 			maxWidth={'xl'}
+			PaperProps={{ sx: { maxHeight: '80vh', minHeight: '80vh', height: '80vh' } }}
 		>
 			<DialogTitle onClick={(e) => e.stopPropagation()}>
-				Set Image for {tag.title}
+				<Stack flexDirection="row" gap="20px">
+					<Typography variant="h6">Set Image for {updatedTag.title}</Typography>
+					{titsQuery.isSuccess && (
+						<TagImageTypeSelector tits={titsQuery.data} tit={tit} onTitChanged={setTit} />
+					)}
+				</Stack>
 				<IconButton
 					sx={{
 						position: 'absolute',
@@ -84,8 +117,10 @@ function ManageTagImageDialog({ tag, onClose }) {
 						objectFit: 'contain',
 						overflow: 'hidden',
 						borderRadius: '5px',
+						height: '100%',
 					}}
 				>
+					<Box></Box>
 					<Box
 						sx={{
 							borderRadius: '5px',
@@ -93,11 +128,11 @@ function ManageTagImageDialog({ tag, onClose }) {
 							overflow: 'hidden',
 						}}
 						component="img"
-						src={TagsUtil.getTagImageUrl(tag, { id: 5 })}
-						alt={tag.title}
+						src={TagsUtil.getTagImageUrl(updatedTag, tit, true)}
+						alt={updatedTag.title}
 						loading="lazy"
 					/>
-					{!hasImage() && (
+					{!TagsUtil.hasImage(updatedTag) && (
 						<Box
 							sx={{
 								position: 'absolute',
@@ -135,10 +170,10 @@ function ManageTagImageDialog({ tag, onClose }) {
 					onClick={(e) => e.stopPropagation()}
 				>
 					<Button
-						disabled={!hasImage()}
+						disabled={!TagsUtil.hasImage(updatedTag)}
 						variant="outlined"
 						onClick={(e) => {
-							if (!hasImage()) {
+							if (!TagsUtil.hasImage(updatedTag)) {
 								return;
 							}
 							removeTagImageClicked(e);
@@ -153,7 +188,15 @@ function ManageTagImageDialog({ tag, onClose }) {
 						}}
 						variant="outlined"
 					>
-						Set Image
+						Upload Image
+					</Button>
+					<Button
+						onClick={(e) => {
+							imageFromUrlClicked(e);
+						}}
+						variant="outlined"
+					>
+						Image From URL
 					</Button>
 				</Box>
 				<Box
